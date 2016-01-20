deffbb28268dfed2c4cbf841455cfb31cc7b113f
yangxinlong <1036140830@qq.com>
2016-01-20 15:32:38 +0800

    添加android项目apple

---
+++ app/src/main/java/jewelry/apple/MainActivity.java
@@ -0,0 +1,65 @@
+package jewelry.apple;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.os.Handler;
+
+
+public class MainActivity extends AppCompatActivity {
+    private TextView text_view = null;
+    private Button start = null;
+    private Button end = null;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text_view = (TextView)findViewById(R.id.text_view);
+        start = (Button)findViewById(R.id.start);
+        start.setOnClickListener(new StartClickListener());
+        end = (Button)findViewById(R.id.end);
+        end.setOnClickListener(new EndClickListener());
+
+    }
+
+    //使用handler时首先要创建一个handler
+    Handler handler = new Handler();
+    //要用handler来处理多线程可以使用runnable接口，这里先定义该接口
+    //线程中运行该接口的run函数
+    Runnable update_Thread = new Runnable()
+    {
+        public void run()
+        {
+            //线程每次执行时输出"UpdateThread..."文字,且自动换行
+            //textview的append功能和Qt中的append类似，不会覆盖前面
+            //的内容，只是Qt中的append默认是自动换行模式
+            text_view.append("\nUpdateThread...");
+            //延时1s后又将线程加入到线程队列中
+            handler.postDelayed(update_Thread, 2000);
+        }
+    };
+
+    private class StartClickListener implements View.OnClickListener
+    {
+        public void onClick(View v) {
+                handler.post(update_Thread);
+                }
+
+        }
+
+
+    private class EndClickListener implements View.OnClickListener
+    {
+        public void onClick(View v) {
+            handler.removeCallbacks(update_Thread);
+        }
+
+    }
+
+}
---
+++ app/src/main/java/jwt/clientHttp.java
@@ -0,0 +1,8 @@
+package jwt;
+
+/**
+ * Created by yang on 16-1-19.
+ */
+
+public class clientHttp {
+}
