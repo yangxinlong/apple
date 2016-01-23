b7bacf83684126c4d9c8fbebeedf367237214730
yangxinlong <1036140830@qq.com>
2016-01-23 17:33:33 +0800

    第三次解除Main3acvitity

--- app/src/main/java/jewelry/apple/Main3Activity.java
+++ app/src/main/java/jewelry/apple/Main3Activity.java
@@ -1,113 +1,101 @@
 package jewelry.apple;
 
 import android.os.Handler;
-import android.os.HandlerThread;
 import android.os.Looper;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class Main3Activity extends AppCompatActivity {
-    Handler mhandler ;
-    private TextView textView;
-    Button btn1;
-    Button btn2;
-    childThread childthread1;
-    childThread childthread2;
-    class childThread extends Thread{
-        private Handler childhandler;
-        private int nClickTimes=0;
+    private Button btn1;
+    private Button btn2;
+    private TextView info;
+    private Handler mhandler;
+    private childThread childThread1;
+    private childThread childThread2;
+
+    class childThread extends Thread {
+        private int ncNumber = 0;
+        private Handler childHandler;
 
         @Override
-        public void run(){
-            this.setName("ChildThread");
-        Looper.prepare();
-            childhandler = new Handler(){
+        public void run() {
+            this.setName("这个是this.setName()");
+            Looper.prepare();
+            childHandler = new Handler() {
                 @Override
                 public void handleMessage(Message msg) {
-
                     try {
-                        //子线程对主线程发过来的数据进行加工
-                        String sMsg="";
-                        sleep(1000);
-                        Message tomain =new Message();
-                        sMsg= String.valueOf(++nClickTimes);
-                        tomain.obj=sMsg+"--childThread--"+msg.obj;
-                        //子线程加工好的数据由主线程Handler负责发送
-                        mhandler.sendMessage(tomain);
+//                        对得到的数据进行加工封装
+                        String sMsg = "";
+                        sleep(500);
+                        sMsg = String.valueOf(++ncNumber);
+                        Message toMain = new Message();
+                        toMain.obj = sMsg + this.getLooper().getThread().getName() + msg;
+//                        加工好的数据由mhandler发送给主线程mhandler
+                        mhandler.sendMessage(toMain);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
             };
-            Looper.loop();
+             Looper.loop();
         }
     }
 
-    View.OnClickListener clickListener1 = new View.OnClickListener() {
+    View.OnClickListener onclicklistener1 = new View.OnClickListener() {
+        /**
+         * Called when a view has been clicked.
+         *
+         * @param v The view that was clicked.
+         */
         @Override
         public void onClick(View v) {
-            if (childthread1.childhandler!=null) {
-                Message msg = childthread1.childhandler.obtainMessage();
-                msg.obj = "mhandler.getLooper().getThread().getName()--::"
-                        +mhandler.getLooper().getThread().getName()
-                        +"由btn1点击出发";
-                msg.sendToTarget();
-            }
+            Message msg = new Message();
+            msg.obj = "bnt1被触发::" + mhandler.getLooper().getThread().getName();
+            childThread1.childHandler.sendMessage(msg);
         }
     };
-    View.OnClickListener clickListener2 =new View.OnClickListener() {
+    View.OnClickListener onclicklistener2 = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            if (childthread2.childhandler!=null){
-                Message msg = new Message();
-                msg.obj="mhandler.getLooper().getThread().getName()--::"
-                        +mhandler.getLooper().getThread().getName()
-                        +"由btn2点击触发";
-//                如果这个地方开始犯晕了用了mhandler那么最后就直接由mhandler来进行数据处理并显示
-                childthread2.childhandler.sendMessage(msg);
-/*                Message msg = childthread2.childhandler.obtainMessage();
-                msg.obj = "mhandler.getLooper().getThread().getName()--::"
-                        +mhandler.getLooper().getThread().getName()
-                        +"由btn2点击出发";
-                msg.sendToTarget();*/
-            }
+            Message msg =new Message();
+            msg.obj = "btn2被触::"+mhandler.getLooper().getThread().getName();
+            childThread2.childHandler.sendMessage(msg);
         }
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
         btn1 = (Button) findViewById(R.id.msgBtn);
         btn2 = (Button) findViewById(R.id.msgBtn2);
-        textView = (TextView) findViewById(R.id.info);
-        mhandler = new Handler(){
+        info = (TextView) findViewById(R.id.info);
+        mhandler = new Handler() {
             @Override
             public void handleMessage(Message msg) {
-                textView.setText((String) msg.obj);
+                info.setText((String) msg.obj);
             }
         };
-        childthread1 = new childThread();
-        childthread2=new childThread();
-        //线程一开启,线程里面设置好的looper就开始不停的旋转,寻找机会找到message队列进行相关的数据处理
-        childthread1.start();
-        childthread2.start();
-//        主线程通过handler向子线程发送数据
-        btn1.setOnClickListener(clickListener1);
-        btn2.setOnClickListener(clickListener2);
+        childThread1 = new childThread();
+        childThread2 = new childThread();
+        childThread1.start();
+        childThread2.start();
+        btn1.setOnClickListener(onclicklistener1);
+        btn2.setOnClickListener(onclicklistener2);
     }
-    @Override
-    public void onDestroy(){
+
+    protected void onDestory() {
         super.onDestroy();
-        if (childthread1.childhandler!=null){
-            childthread1.childhandler.getLooper().quit();
+        if (childThread1.childHandler != null) {
+            childThread1.childHandler.getLooper().quit();
         }
-        if (childthread2.childhandler!=null){
-            childthread2.childhandler.getLooper().quit();
+        if (childThread2.childHandler != null) {
+            childThread2.childHandler.getLooper().quit();
         }
     }
 }
